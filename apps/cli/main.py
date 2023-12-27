59b8f432bbda60efedd9dceffcb7ca82454896e6
Nikodem Wrona <wronanikodem@gmail.com>
2023-12-27 21:35:11 +0100

    feat: add python script to inject books into database

--- apps/cli/main.py
+++ apps/cli/main.py
@@ -1,18 +1,63 @@
-import sys
+import requests
+import json
+import time
 
-from rich.console import Console
+def open_file_with_books(): 
+    books = []
+    
+    with open('data.txt', 'r') as file:
+        for line in file:
+            line = line.strip()
+
+            if not line:
+                continue
+
+            parts = line.split(',')
+            parts = [part.strip() for part in parts]
+            
+            book = {
+                'title': parts[0],
+                'author': parts[1],
+                'isFinished': True if parts[2].lower() == 'true' else False,
+            }
+            
+            books.append(book)
+            
+    return books
+
+def send_graphql_mutation(books):
+    url = 'http://localhost:8000/graphql'
+    headers = {'Content-Type': 'application/json', 'x-api-key': ''}
+
+    for book in books:
+        mutation = """
+        mutation {
+            createBook(input: {title: "%s", author: "%s", isFinished: %s}) {
+                book {
+                    id
+                    title
+                    author
+                    isFinished
+                }
+            }
+        }
+        """ % (book['title'], book['author'], book['isFinished'])
+
+        body = {'query': mutation}
+        
+        response = requests.post(url, headers=headers, json=body)
+
+        # Parse the response
+        data = json.loads(response.text)
+
+        # Print the response
+        print("sent : ", data)
+        time.sleep(2)
 
 def main():
-    # sys.argv[0] is the script name itself
-    for i in range(1, len(sys.argv)):
-        print(f"Argument {i} is: {sys.argv[i]}")
-        
-    console = Console()
-
-    console.print([1, 2, 3])
-    console.print("[blue underline]Looks like a link")
-    console.print("FOO", style="white on blue")
-
+    books = open_file_with_books()
+    
+    print(books)
 
 if __name__ == "__main__":
     main()
